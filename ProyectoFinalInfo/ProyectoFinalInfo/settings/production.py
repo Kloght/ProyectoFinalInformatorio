89e3877d489755f77fb571e0902f3548e8153479
Mauricio Alejandro Pérez <map.j91@gmail.com>
2022-09-01 03:11:54 -0300

    listo

--- ProyectoFinalInfo/ProyectoFinalInfo/settings/production.py
+++ ProyectoFinalInfo/ProyectoFinalInfo/settings/production.py
@@ -27,8 +27,16 @@ db_from_env= dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 django_heroku.settings(locals())
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  
+STATIC_URL = '/static/'
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (  
+    os.path.join(BASE_DIR, 'static'),
+)
+
 STATICFILES_STORAGE='whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATICFILES_STORAGE='whitenoise.storage.CompressedStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 
 STATIC_URL = 'https://django-postgres.herokuapp.com/static/'
